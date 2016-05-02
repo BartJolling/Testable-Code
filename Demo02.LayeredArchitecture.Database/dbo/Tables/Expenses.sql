3bacbffdd5fe4d8315445835decf7c77624c53cb
BartJolling <bart.jolling@outlook.com>
2016-05-02 11:19:34 +0100

    Primary key of Expenses table is combination of EmpId, Cat and Year

--- Demo02.LayeredArchitecture.Database/dbo/Tables/Expenses.sql
+++ Demo02.LayeredArchitecture.Database/dbo/Tables/Expenses.sql
@@ -2,7 +2,7 @@
     [EmployeeId]  INT              NOT NULL,
     [FiscalYear]  INT              NOT NULL,
     [Category]    VARCHAR (25)     NOT NULL,
-	[Amount]      NUMERIC (18, 2)  NOT NULL,
-    PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+	[Amount]      NUMERIC (18, 2)  NOT NULL, 
+    CONSTRAINT [PK_Expenses] PRIMARY KEY ([EmployeeId],[FiscalYear],[Category])
 );
 
